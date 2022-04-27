fde17e215b615d214ae53e6aaca68aa082c7692a
Adrian <adrianuta48@gmail.com>
2022-04-28 02:52:05 +0300

    seminar7

---
+++ sem7_prob10/src/clase/CardStb.java
@@ -0,0 +1,10 @@
+package clase;
+
+
+public class CardStb implements ModPlata{
+    @Override
+    public void plateste(float suma) {
+        System.out.println("Plata efectuata cu cardul STB si suma de "+suma);
+
+    }
+}
